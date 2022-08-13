2a1ed26e89060fb10aeea0a2d2aa4754504614f7
Atabek04 <otabek.shadimatov@gmail.com>
2022-08-13 21:13:10 +0600

    new changes

--- store/views.py
+++ store/views.py
@@ -1,13 +1,46 @@
 from django.shortcuts import render
+from .models import *
 
 def storeHandler(request):
+	products = Product.objects.all()
 
-	context = {}
+	context = {
+		'products':products
+	}
 	return render(request, 'store/store.html', context)
 
 def cartHandler(request):
-	return render(request, 'store/cart.html', )
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		items = []
+		order = {
+			'get_cart_total':0,
+			'get_cart_items':0
+		}
+	context = {
+		'items':items,
+		'order':order,
+	}
+	return render(request, 'store/cart.html', context)
 
 def checkoutHandler(request):
-	return render(request, 'store/checkout.html')
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		items = []
+		order = {
+			'get_cart_total':0,
+			'get_cart_items':0
+		}
+	context = {
+		'items':items,
+		'order':order,
+	}
+	return render(request, 'store/checkout.html', context)
 
